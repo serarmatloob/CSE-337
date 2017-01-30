3efd9bef152e343e6d9738f4a4122c73ea61b9ca
Serar Matloob <serarmatloob@oakland.edu>
2017-01-30 00:27:21 -0500

    Update BearSong.java
    Added empty main() and Ninety_Nine_Bottles_of_Beer() method

--- BearSong.java
+++ BearSong.java
@@ -1,1 +1,12 @@
-
+public class BearSong {
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    public void Ninety_Nine_Bottles_of_Beer(){
+        
+    }
+}
